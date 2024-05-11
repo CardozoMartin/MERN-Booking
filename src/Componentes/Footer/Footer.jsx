d9fd043280a4c84a3a48a92a915a4249dad4d639
martin cardozo <tiin.-@hotmail.es>
2024-05-11 15:23:45 -0300

    se agrega footer

--- src/Componentes/Footer/Footer.jsx
+++ src/Componentes/Footer/Footer.jsx
@@ -2,7 +2,38 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="fcontainer">
+
+    <div className='footer'>
+      <div className="fLists">
+        <ul className="fList">
+          <li className="fLisItem">Countries</li>
+          <li className="fLisItem">Regions</li>
+          <li className="fLisItem">Cities </li>
+          <li className="fLisItem">Districts </li>
+          <li className="fLisItem">Airports </li>
+          <li className="fLisItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fLisItem">Countries</li>
+          <li className="fLisItem">Regions</li>
+          <li className="fLisItem">Cities </li>
+          <li className="fLisItem">Districts </li>
+          <li className="fLisItem">Airports </li>
+          <li className="fLisItem">Hotels</li>
+        </ul>
+        <ul className="fList">
+          <li className="fLisItem">Countries</li>
+          <li className="fLisItem">Regions</li>
+          <li className="fLisItem">Cities </li>
+          <li className="fLisItem">Districts </li>
+          <li className="fLisItem">Airports </li>
+          <li className="fLisItem">Hotels</li>
+        </ul>
+        
+      </div>
+    </div>
+    </div>
   )
 }
 
